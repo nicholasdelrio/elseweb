49c28f01d21835d446a8ca5ee466ec9335615848
Nicholas Del Rio <nicholas.delrio@gmail.com>
2014-03-23 19:40:49 -0400

    added publications

---
+++ elseweb.cybershare.utep.edu/site/application/views/pages/publications.php
@@ -0,0 +1,23 @@
+    <!--container start-->
+    <div class="container">
+
+        <div class="row">
+            <!--feature start-->
+            <div class="text-center feature-head">
+                <h2>Papers</h2>
+            </div>
+            <!--feature end-->
+        </div>
+
+   <div class="row gray-box">
+        <div class="hiring">
+            <div class="col-lg-6 col-sm-6">
+                <div class="content">
+                <p>
+                Del Rio, N., Villanueva-Rosales, N., Pennington, D., Benedict, K., Stewart, A., & Grady, C. J. (2013, December). Elseweb meets sadi: Supporting data-to-model integration for biodiversity forecasting. In Discovery Informatics Symposium.
+                </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--container end-->
