09b768c0cbbb2e767224b38f0a31382573d11beb
Nicholas Del Rio <nicholas.delrio@gmail.com>
2014-03-23 12:17:17 -0500

    updating website

---
+++ elseweb.cybershare.utep.edu/site/application/controllers/team.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Team extends MY_Controller {
+	
+	public function index($renderData=""){	
+		
+		/*
+		 *set up title and keywords (if not the default in custom.php config file will be set) 
+		 */
+         
+         $this->title = "ELSEWeb | Team";
+		
+		
+        // 1. when you pass AJAX to renderData it will generate only that particular PAGE skipping other parts like header, nav bar,etc.,
+        //      this can be used for AJAX Responses
+        // 2. when you pass JSON , then the response will be json object of $this->data.  This can be used for JSON Responses to AJAX Calls.
+        // 3. By default full page will be rendered
+        $folder = 'template';
+		$this->_render('pages/team',$renderData,'template');
+	}
+	
+}
---
+++ elseweb.cybershare.utep.edu/site/application/views/pages/team.php
@@ -0,0 +1,46 @@
+     <div class="container">
+
+        <div class="row text-center">
+            
+            <div class="text-center feature-head">
+                <h1> The ELSEWeb Team</h1>
+                <p>An eclectic blend of technical skills.</p>
+            </div>
+            <div class="col-lg-4">
+                <div class="person text-center">                   
+                    <img src="<?php echo base_url(IMAGES."team/dpennington.jpg");?>" alt="image">
+                </div>
+                <div class="person-info text-center">
+                    <h4>
+                        <a href="javascript:;">Deana Penningon</a>
+                    </h4>
+                    <p class="text-muted">Principal Investigator</p>
+                    <div class="team-social-link">
+                        <a href="https://www.facebook.com/deana.pennington"><i class="icon-facebook"></i></a>
+                    </div>
+                    <p>Interdisciplinary knowledge exchange processes, Geospatial, Data Science</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                 <div class="person text-center">
+                    <img src="<?php echo base_url(IMAGES."team/ndelrio.jpg");?>" alt="image">
+                </div>
+                <div class="person-info text-center">
+                    <h4>
+                        <a href="javascript:;">Nicholas Del Rio</a>
+                    </h4>
+                    <p class="text-muted">Lead Design and Developer</p>
+                    <div class="team-social-link">
+                        <a href="https://www.facebook.com/nicholas.delrio"><i class="icon-facebook"></i></a>
+                        <a href="https://twitter.com/NicholasDelRio"><i class="icon-twitter"></i></a>
+                        <a href="https://plus.google.com/+NicholasDelRio"><i class="icon-google-plus"></i></a>
+                    </div>
+                    <p>Semantic Web, Visualization, and Provenance</p>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+    <!--container end-->
