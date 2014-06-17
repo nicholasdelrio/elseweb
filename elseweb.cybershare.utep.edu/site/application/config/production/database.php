944fa9838e4b031700e41421f19a6e4904d8e119
Luis Garnica <lagc03@gmail.com>
2014-06-17 16:00:55 -0600

    Production Beta Still needs URL change on production config

--- elseweb.cybershare.utep.edu/site/application/config/production/database.php
+++ elseweb.cybershare.utep.edu/site/application/config/production/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = '';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['hostname'] = 'visko.cybershare.utep.edu';
+$db['default']['username'] = 'elseweb';
+$db['default']['password'] = '3ls3w3b';
+$db['default']['database'] = 'elseweb_experiments';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
