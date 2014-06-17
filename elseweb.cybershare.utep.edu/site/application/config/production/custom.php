53026e8119d8997842b8aee1bdf9d7f49231ce76
Luis Garnica <lagc03@gmail.com>
2014-06-17 13:47:05 -0600

    + Fixed production bug. + Added production config files.

---
+++ elseweb.cybershare.utep.edu/site/application/config/production/custom.php
@@ -0,0 +1,29 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Base Site URL
+|--------------------------------------------------------------------------
+|
+| URL to your CodeIgniter root. Typically this will be your base URL,
+| WITH a trailing slash:
+|
+|	http://example.com/
+|
+| If this is not set then CodeIgniter will guess the protocol, domain and
+| path to your installation.
+|
+*/
+//$config['base_url']	= 'http://elseweb.cybershare.utep.edu/';
+$config['base_url'] = 'http://localhost/elseweb/elseweb.cybershare.utep.edu/site/';
+
+
+
+$config['site_title'] = 'ELSEWeb';
+$config['site_description'] = 'The Earth, Life and Semantic Web';
+$config['site_author'] = 'UTEP Cybershare';
+$config['site_keywords'] = 'elseweb, cybershare, species modeling, species modelling';
+
+
+/* End of file custom.php */
+/* Location: ./application/config/production/custom.php */
